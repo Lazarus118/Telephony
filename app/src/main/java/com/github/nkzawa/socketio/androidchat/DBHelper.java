b2dadced2956f8c53910be5b7488a736f8f9d394
Austin Lazarus <austin.lazarus@gmail.com>
2016-10-26 18:48:00 -0400

    commit - 26.10.16

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/DBHelper.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/DBHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "contacts.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
     public static final String TABLE_NAME = "contacts";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_NAME = "name";
